9389fbbc6e3ebd08e340982f84ea4dea2d67efe2
Clement Gauthier <clmt.gauthier@gmail.com>
2018-05-31 11:26:33 +0200

    define get_or_create as URL class method

--- urlshortener/shortener/models.py
+++ urlshortener/shortener/models.py
@@ -10,20 +10,20 @@ class URL(models.Model):
     url = models.TextField()
     alias = models.CharField(max_length=4, primary_key=True)
 
+    @classmethod
+    def get_or_create(cls, url):
+        # create an SHA1 for the current url
+        hasher = hashlib.sha1(url.encode())
+        # Alias should be 4 character long
+        alias = base64.urlsafe_b64encode(hasher.digest())[0:4]
 
-def get_or_create_url(url):
-    # create an SHA1 for the current url
-    hasher = hashlib.sha1(url.encode())
-    # Alias should be 4 character long
-    alias = base64.urlsafe_b64encode(hasher.digest())[0:4]
+        # retieve URL object from db if exists to avoid colision
+        obj, _ = cls.objects.get_or_create(alias=alias)
 
-    # retieve URL object from db if exists to avoid colision
-    obj, _ = URL.objects.get_or_create(alias=alias)
+        # Don't update if same url is given
+        if url != obj.url:
+            obj.url = url
+            obj.save()
 
-    # Don't update if same url is given
-    if url != obj.url:
-        obj.url = url
-        obj.save()
-
-    # Return current instance, hope it will be usefull :D
-    return obj
+        # Return current instance, hope it will be usefull :D
+        return obj
