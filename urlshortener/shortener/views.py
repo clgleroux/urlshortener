f7f71005ec523e60e8b9d78f0ce7bf80c3a038cd
Clement Gauthier <clmt.gauthier@gmail.com>
2018-05-31 12:58:20 +0200

    [demo] simple view example

--- urlshortener/shortener/views.py
+++ urlshortener/shortener/views.py
@@ -3,4 +3,6 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
-# Create your views here.
+
+def simple(request):
+    return render(request, "shortener/hello.html", {'name': 'Buddy'})
